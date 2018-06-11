3ea6c115a3a795c1ea40fd83ca4b86992e72a83a
lytempel <lytempel@gmail.com>
2018-06-11 17:14:28 +0300

    Order products on click by name, store and stock.

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,7 @@ app.controller('productsCtrl', function($scope, $http) {
     $http.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d').then(function (response) {
        $scope.products = response.data;
     });
+    $scope.orderByFun = function(x) {
+        $scope.orderByClick = x;
+    }
 });
