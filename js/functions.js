967a4eaaf6d53167e35094fe8e8ca20216df6344
lytempel <lytempel@gmail.com>
2018-06-19 12:58:49 +0300

    Primary design, navigation bar, showing product details in the modal window.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,18 @@
 var app = angular.module('app', []);
 app.controller('productsCtrl', function($scope, $http) {
-    $http.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d').then(function (response) {
-       $scope.products = response.data;
+    $http.get('https://erply-challenge.herokuapp.com/list?AUTH=fae7b9f6-6363-45a1-a9c9-3def2dae206d')
+    .then(function (response) {
+        $scope.products = response.data;
+        $scope.requestProductInfo = function(id) {
+            $scope.productInfo = response.data[id-1];
+        };
+
+    }, function(response) {
+        $scope.products = "Something went wrong";
     });
     $scope.orderByFun = function(x) {
         $scope.orderByClick = x;
-    }
+    };
+
 });
+
